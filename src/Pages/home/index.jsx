828759ec1105ddebc78405e2b906e2a5901f4de7
Felipe Pinto <felipepinto.fpm@gmail.com>
2020-07-31 21:17:02 -0300

    Ajuste de ERRO ao carregar o carrosel

--- src/Pages/home/index.jsx
+++ src/Pages/home/index.jsx
@@ -1,20 +1,26 @@
-import React from 'react';
-import {HomeWrapper} from "../styled";
+import React from "react";
+import { HomeWrapper } from "../styled";
+import dadosIniciais from "../../Data/dados_iniciais.json";
 
-import Menu from '../../Components/Menu';
-import BannerMain from '../../Components/BannerMain'
-import Carousel from '../../Components/Carousel'
-import Footer from '../../Components/Footer'
+import Menu from "../../Components/Menu";
+import BannerMain from "../../Components/BannerMain";
+import Carousel from "../../Components/Carousel";
+import Footer from "../../Components/Footer";
 
-function Home(){
-    return (
-        <HomeWrapper>
-            <Menu />
-            <BannerMain />
-            <Carousel />
-            <Footer />
-        </HomeWrapper>
-    )
+function Home() {
+  return (
+    <HomeWrapper>
+      <Menu />
+      <BannerMain />
+      <Carousel ignoreFirstVideo category={dadosIniciais.categorias[0]} />
+      <Carousel category={dadosIniciais.categorias[1]} />
+      <Carousel category={dadosIniciais.categorias[2]} />
+      <Carousel category={dadosIniciais.categorias[3]} />
+      <Carousel category={dadosIniciais.categorias[4]} />
+      <Carousel category={dadosIniciais.categorias[5]} />
+      <Footer />
+    </HomeWrapper>
+  );
 }
 
 export default Home;
